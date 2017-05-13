77f19d7e60dcdfffe12090bcb026e0a552a057b7
andy tran <andytran78@hotmail.com>
2017-05-13 09:44:37 -0400

    Final

---
+++ bamazonManager.js
@@ -0,0 +1,172 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1111",
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log('Welcome to Bamazon!');
+    managerTasks();
+});
+
+function managerTasks() {
+
+    inquirer.prompt([{
+
+            type: 'list',
+            name: 'tasks',
+            message: 'What would you like to do?',
+            choices: ['view-products-for-sale', 'view-low-inventory', 'add-to-inventory', 'add-new-product', 'quit']
+        }
+
+    ]).then(function(choice) {
+
+        if (choice.tasks === 'view-products-for-sale') {
+            viewProducts();
+
+        } else if (choice.tasks === 'view-low-inventory') {
+            viewLowInventory();
+        } else if (choice.tasks === 'add-to-inventory') {
+            addToInventory();
+        } else if (choice.tasks === 'add-new-product') {
+            addNewProduct();
+        } else {
+
+            connection.end(function(err) {
+            });
+
+        }
+    });
+}
+//Displays current products in store
+function viewProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log('\n');
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + '. ' + res[i].product_name + ' | ' + res[i].department_name + ' | ' + '$' +
+                res[i].price.toFixed(2) + ' | ' + res[i].stock_quantity);
+            console.log('----------------------------------------------------------');
+        }
+        managerTasks();
+    });
+
+};
+//Checks for products with fewer than five units in stock
+function viewLowInventory() {
+
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+        console.log('\n');
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + '. ' + res[i].product_name + ' | ' + res[i].department_name + ' | ' + '$' +
+                res[i].price.toFixed(2) + ' | ' + res[i].stock_quantity);
+            console.log('----------------------------------------------------------');
+        }
+        managerTasks();
+    })
+
+};
+//Allows manager to add more units to inventory
+var addToInventory = function() {
+    connection.query("SELECT product_name,stock_quantity FROM products", function(err, res) {
+
+        inquirer.prompt({
+            name: "products",
+            type: "rawlist",
+            choices: function(value) {
+                var productsArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    productsArray.push(res[i].product_name);
+                }
+                return productsArray;
+            },
+            message: "Which product would you like to restock?"
+        }).then(function(answer) {
+            for (var i = 0; i < res.length; i++) {
+
+                if (res[i].product_name === answer.products) {
+                    var chosenItem = res[i];
+                    inquirer.prompt({
+                        name: "quantity",
+                        type: "input",
+                        message: "You currently have " + chosenItem.stock_quantity + " of that item. How many units would you like to add?"
+                    }).then(function(ans) {
+
+                        var total = chosenItem.stock_quantity + parseInt(ans.quantity);
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: total
+                        }, {
+                            product_name: chosenItem.product_name
+                        }], function(err, res) {
+                            if (err) {
+                                throw err;
+                            } else {
+                                const maybePluralize = (count, noun, suffix = 's') =>
+                                    `${count} ${noun}${count !== 1 && noun.charAt(noun.length-1)!== 's'? suffix : ''}`;
+                                console.log("You now have " + maybePluralize(total, chosenItem.product_name) + ' in stock.');
+
+                                managerTasks();
+                            }
+                        });
+
+                    })
+                }
+
+            }
+        })
+    })
+}
+
+//Allows manager to add a new product
+function addNewProduct() {
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "What product would you like to add?"
+    }, {
+        name: "department",
+        type: "input",
+        message: "In which department does the item belong?"
+    }, {
+        name: "price",
+        type: "input",
+        message: "What is the price per unit?",
+        validate: function(value) {
+            if (isNaN(value) === false && value.length > 0) {
+                return true;
+            }
+            return false;
+        }
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many units would you like to add?",
+        validate: function(value) {
+            if (isNaN(value) === false && value.length > 0) {
+                return true;
+            }
+            return false;
+        }
+
+    }]).then(function(answer) {
+        connection.query("INSERT INTO products SET ?", {
+            product_name: answer.product,
+            department_name: answer.department,
+            price: answer.price,
+            stock_quantity: answer.quantity
+        }, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            console.log("Your product was added successfully!");
+            managerTasks();
+        });
+    });
+
+};
---
+++ bamazonSupervisor.js
@@ -0,0 +1,101 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1111",
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    taskChoice();
+});
+
+function taskChoice() {
+    inquirer.prompt([{
+
+            type: 'list',
+            name: 'task',
+            message: 'What would you like to do?',
+            choices: ['view-product-sales-by-department', 'create-new-department', 'quit']
+        }
+
+    ]).then(function(choice) {
+
+        if (choice.task === 'view-product-sales-by-department') {
+            viewProductSales();
+
+        } else if (choice.task === 'create-new-department') {
+            createNewDepartment();
+
+        } else {
+            console.log('Session ended');
+            connection.end(function(err) {
+                // The connection is terminated gracefully
+
+            });
+
+        }
+    });
+}
+
+function viewProductSales() {
+    //Using an alias, this query creates a new column on the fly to reflect the actual total profits of Bamazon by department (total sales - overhead costs)
+    connection.query("SELECT total_sales - over_head_costs AS total_profit,department_id,department_name,over_head_costs,total_sales FROM departments", function(err, res) {
+
+        // instantiates the table object
+        var table = new Table({
+            head: ['ID', 'department_name', 'over_head_costs', 'total_sales', 'total_profit'],
+            colWidths: [5, 20, 20, 20, 20]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            // pushes the query results into the table
+            table.push(
+                [res[i].department_id, res[i].department_name, res[i].over_head_costs.toFixed(2), res[i].total_sales.toFixed(2), res[i].total_profit.toFixed(2)]
+            );
+        }
+        console.log(table.toString());
+        taskChoice();
+    });
+}
+//allows Supervisor to create a new department. In the schema, default value for total sales is set to zero
+function createNewDepartment() {
+    inquirer.prompt([{
+        name: "department",
+        type: "input",
+        message: "What is the department name?",
+        validate: function(value) {
+            if (value.length > 0) {
+                return true;
+            }
+            return false;
+        }
+    }, {
+
+        name: "costs",
+        type: "input",
+        message: "What are the overhead costs?",
+        validate: function(value) {
+            if (isNaN(value) === false && value !== 0 && value.length > 0) {
+                return true;
+            }
+            return false;
+        }
+    }]).then(function(answer) {
+        connection.query("INSERT INTO departments SET ?", {
+            department_name: answer.department,
+            over_head_costs: answer.costs
+        }, function(err, res) {
+            if (err) throw err;
+            console.log("The department was added successfully.");
+            taskChoice();
+        });
+    });
+
+};
